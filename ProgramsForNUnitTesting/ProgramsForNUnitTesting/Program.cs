bdf1a3084a112e68dba0e3eca7222e19172ac4fd
Manoj Kumar Manukonda <manojkumarmanukonda77@gmail.com>
2022-11-05 09:13:55 +0530

    [Manoj] Add: Vending Machine

--- ProgramsForNUnitTesting/ProgramsForNUnitTesting/Program.cs
+++ ProgramsForNUnitTesting/ProgramsForNUnitTesting/Program.cs
@@ -2,9 +2,24 @@
 {
     public class Program
     {
-        static void main(string[] args)
+        static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Programs for NUnit Testing problems");
+            bool flag = true;
+            Console.WriteLine(" 1.VendingMachine");
+            int check = Convert.ToInt32(Console.ReadLine());
+            while (flag)
+            {
+                switch (check)
+                {
+                    case 1:
+                        VendingMachine vendingMachine = new VendingMachine();
+                        vendingMachine.Notes(8);
+                        vendingMachine.Print();
+                        break;
+                }
+                break;
+            }
         }
     }
 }
